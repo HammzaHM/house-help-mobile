273e63b9a723ecc863a04117caa508c24f55083f
HammzaHM <hmemm.hamza1@gmail.com>
2022-01-08 10:10:37 +0100

    The first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# House Help Mobile Application
+React Native Application that helps Tunisian citizens to find the best quality daily services with a simple click
---
+++ app.config.ts
@@ -0,0 +1,31 @@
+import { ExpoConfig } from '@expo/config-types';
+
+const config: ExpoConfig = {
+    name: "houseHelp",
+    slug: "houseHelp",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    updates: {
+      "fallbackToCacheTimeout": 0
+    },
+    assetBundlePatterns: [
+      "**/*"
+    ],
+    ios: {
+      "supportsTablet": true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#FFFFFF"
+      }
+    },
+};
+
+export default config;
